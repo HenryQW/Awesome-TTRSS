8fea4cf3768c9c80e145b6bb3a4717ef1902df60
oucb <oucb@outlook.com>
2018-08-21 21:56:22 +0800

    fix(configure-db.php): fix that can't create database's table

--- configure-db.php
+++ configure-db.php
@@ -13,41 +13,50 @@ $config['DB_NAME'] = env('DB_NAME', 'ttrss');
 $config['DB_USER'] = env('DB_USER');
 $config['DB_PASS'] = env('DB_PASS');
 
-if(dbcheckconn($config)){
-    $pdo = dbconnect($config);
-
-    if(!dbcheckdb($config)){
-
-        echo 'Database not found, creating.'. PHP_EOL ;
-
-        $pdo = dbconnect($config);
-        $pdo -> exec('CREATE DATABASE ' . ($config['DB_NAME']) . ' WITH OWNER ' . ($config['DB_USER']));
-
-        unset($pdo);
-
-        $pdo = dbexist($config);
-        try {
-            $pdo->query('SELECT 1 FROM ttrss_feeds');
-        }
-        catch (PDOException $e) {
-            echo 'Database table not found, applying schema... ' . PHP_EOL;
-            $schema = file_get_contents('schema/ttrss_schema_' . $config['DB_TYPE'] . '.sql');
-            $schema = preg_replace('/--(.*?);/', '', $schema);
-            $schema = preg_replace('/[\r\n]/', ' ', $schema);
-            $schema = trim($schema, ' ;');
-            foreach (explode(';', $schema) as $stm) {
-                $pdo->exec($stm);
-            }
-            unset($pdo);
-        }
+if(!dbcheckconn($config)){
+    echo 'Database login failed, trying to create...' . PHP_EOL;
+    // superuser account to create new database and corresponding user account
+    //   username (SU_USER) can be supplied or defaults to "docker"
+    //   password (SU_PASS) can be supplied or defaults to username
+    $super = $config;
+    $super['DB_NAME'] = null;
+    $super['DB_USER'] = env('DB_ENV_USER', 'docker');
+    $super['DB_PASS'] = env('DB_ENV_PASS', $super['DB_USER']);
+    
+    $pdo = dbconnect($super);
+    $pdo->exec('CREATE ROLE ' . ($config['DB_USER']) . ' WITH LOGIN PASSWORD ' . $pdo->quote($config['DB_PASS']));
+    $pdo->exec('CREATE DATABASE ' . ($config['DB_NAME']) . ' WITH OWNER ' . ($config['DB_USER']));
+    unset($pdo);
+    
+    if (dbcheck($config)) {
+        echo 'Database login created and confirmed' . PHP_EOL;
+    } else {
+        error('Database login failed, trying to create login failed as well');
     }
-    $contents = file_get_contents($confpath);
-    foreach ($config as $name => $value) {
-        $contents = preg_replace('/(define\s*\(\'' . $name . '\',\s*)(.*)(\);)/', '$1"' . $value . '"$3', $contents);
-    }
-    file_put_contents($confpath, $contents);
 }
 
+$pdo = dbconnect($config);
+try {
+    $pdo->query('SELECT 1 FROM ttrss_feeds');
+    // reached this point => table found, assume db is complete
+}
+catch (PDOException $e) {
+    echo 'Database table not found, applying schema... ' . PHP_EOL;
+    $schema = file_get_contents('schema/ttrss_schema_' . $config['DB_TYPE'] . '.sql');
+    $schema = preg_replace('/--(.*?);/', '', $schema);
+    $schema = preg_replace('/[\r\n]/', ' ', $schema);
+    $schema = trim($schema, ' ;');
+    foreach (explode(';', $schema) as $stm) {
+        $pdo->exec($stm);
+    }
+    unset($pdo);
+}
+
+$contents = file_get_contents($confpath);
+foreach ($config as $name => $value) {
+    $contents = preg_replace('/(define\s*\(\'' . $name . '\',\s*)(.*)(\);)/', '$1"' . $value . '"$3', $contents);
+}
+file_put_contents($confpath, $contents);
 
 function env($name, $default = null)
 {
@@ -84,31 +93,6 @@ function dbcheckconn($config)
 {
     try {
         dbconnect($config);
-        return true;
-    }
-    catch (PDOException $e) {
-        return false;
-    }
-}
-
-function dbexist($config)
-{
-    $map = array('host' => 'HOST', 'port' => 'PORT' , 'dbname' =>'NAME');
-    $dsn = $config['DB_TYPE'] . ':';
-    foreach ($map as $d => $h) {
-        if (isset($config['DB_' . $h])) {
-            $dsn .= $d . '=' . $config['DB_' . $h] . ';';
-        }
-    }
-    $pdo = new \PDO($dsn, $config['DB_USER'], $config['DB_PASS']);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $pdo;
-}
-
-function dbcheckdb($config)
-{
-    try {
-        dbexist($config);
         return true;
     }
     catch (PDOException $e) {
