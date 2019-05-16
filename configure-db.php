d3a61e609217bea2594f121df6b954622a00f2c3
KK <bebound@users.noreply.github.com>
2019-05-16 15:58:48 +0800

    Set session expire time to 365 days

--- configure-db.php
+++ configure-db.php
@@ -13,6 +13,7 @@ $config['DB_NAME'] = env('DB_NAME', 'ttrss');
 $config['DB_USER'] = env('DB_USER');
 $config['DB_PASS'] = env('DB_PASS');
 $config['PLUGINS'] = env('ENABLE_PLUGINS','auth_internal,fever');
+$config['SESSION_COOKIE_LIFETIME'] = env('SESSION_COOKIE_LIFETIME',86400 * 365);
 
 if(dbcheckconn($config)){
     $pdo = dbconnect($config);
