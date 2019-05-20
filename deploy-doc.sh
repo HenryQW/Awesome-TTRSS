34292c83b3d2e19a63ace4fc8041e57b84f2b984
Henry <hi@henry.wang>
2019-05-20 19:51:56 +0100

    chore: update deploy-doc script

--- deploy-doc.sh
+++ deploy-doc.sh
@@ -8,12 +8,9 @@ cd docs/.vuepress/dist
 
 echo 'ttrss.henry.wang' >CNAME
 
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
-
 git init
 git add -A
-git commit -m "docs:deploy v$TRAVIS_BUILD_NUMBER"
+git -c "user.name=Travis CI" -c "user.email=travis@travis-ci.org" commit -m "docs:deploy v$TRAVIS_BUILD_NUMBER"
 
 git push -f https://${GITHUB_TOKEN}@github.com/HenryQW/Awesome-TTRSS.git master:gh-pages
 
