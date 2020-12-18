d7a76e9a31abc5d39da0b92be3277bbc39f61dc4
Henry <hi@henry.wang>
2020-12-18 16:14:43 +0000

    fix: ALLOW_PORTS in urlhelper.php
    close #232

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,14 +11,13 @@ if [ "$ALLOW_PORTS" != "80,443" ]; then
     sed -i -r "s/(80, 443).*?('')/$ALLOW_PORTS/" /var/www/classes/urlhelper.php
 
     # modify BL to include ports
-    CODE="if (isset(\$parts['port'])) {\$tmp .= ':' . \$parts['port'];} \n \$tmp = \$tmp . \$parts['path'];"
-    sed -i "/\$parts\['path'\];/a $CODE" /var/www/classes/urlhelper.php
-    sed -i -r "s/\]\ \.\ \\\$parts\['path'\]/\]/" /var/www/classes/urlhelper.php
+    CODE="if (isset(\$parts['port'])) \$tmp .= ':' . \$parts['port']; \n"
+    sed -i "/if (isset(\$parts\['path'\]))/i $CODE" /var/www/classes/urlhelper.php
 fi
 
-if [ -n "$DB_USER_FILE" ]; then DB_USER="$(cat $DB_USER_FILE)"; fi;
+if [ -n "$DB_USER_FILE" ]; then DB_USER="$(cat $DB_USER_FILE)"; fi
 
-if [ -n "$DB_PASS_FILE" ]; then DB_PASS="$(cat $DB_PASS_FILE)"; fi;
+if [ -n "$DB_PASS_FILE" ]; then DB_PASS="$(cat $DB_PASS_FILE)"; fi
 
 sh /wait-for.sh $DB_HOST:$DB_PORT -- php /initialize.php && exec s6-svscan /etc/s6/
 
