bb48cff3c2a0573386811d29de5e07841f592884
Henry <hi@henry.wang>
2021-02-15 15:37:08 +0000

    chore: rename master to main branch

--- deploy-doc.sh
+++ deploy-doc.sh
@@ -8,4 +8,4 @@ git init
 git add -A
 git -c "user.name=GitHub Actions" -c "user.email=actions@github.com" commit -m "docs:deploy $(date '+%Y-%m-%d %H:%M:%S')"
 
-git push -f https://${CI_TOKEN}@github.com/HenryQW/Awesome-TTRSS.git master:gh-pages
+git push -f https://${CI_TOKEN}@github.com/HenryQW/Awesome-TTRSS.git main:gh-pages
